14f9b4fec285e140baf72c5bd0e96d6cf978d003
shokry1337 <emmess1337@gmail.com>
2026-02-12 01:46:58 +0200

    Update vinspecialistenaalborg.dk

--- services/refiners/sites/vinspecialistenaalborg.dk.js
+++ services/refiners/sites/vinspecialistenaalborg.dk.js
@@ -1,26 +1,20 @@
 // services/refiners/sites/vinspecialistenaalborg.dk.js
-
 export default async function refine(rootUrl, product, page) {
+    product.country = 'Denmark';
+    product.currency = 'DKK';
+    product.price = product.price
+        .replace(',', '.')
+        .replace(' DKK', '')
+        .trim
+    product.name = product.name.replace(' - Alkoholfri - Vinspecialisten Aalborg', '').trim();
+    product.description = product.description.replace(' […]', '').trim();
 
-  /**
-   * ====================
-   * ABV — from product name
-   * ====================
-   * Example:
-   * "Torres, Natureo Rosé, Cabernet Sauvignon, 0.75 l. – 0.0%"
-   */
-  if (product.name) {
+
     const match = product.name.match(/(\d+(?:[.,]\d+)?)\s*%/);
     if (match) {
-      // Normalize Danish / EU decimal
-      product.abv = `${match[1].replace(',', '.')}%`;
+        product.abv = `${match[1].replace(',', '.')}%`;
     } else {
-      product.abv = null;
+        product.abv = null;
     }
-  } else {
-    product.abv = null;
-  }
-
-  return product;
+    return product;
 }
-
