445f847a12262a84390cede622d252c221870220
shokry1337 <emmess1337@gmail.com>
2026-03-16 03:23:33 +0200

    Update dhwines.dk

--- services/refiners/sites/dhwines.dk.js
+++ services/refiners/sites/dhwines.dk.js
@@ -13,5 +13,31 @@ export default async function refine(rootUrl, product, page) {
     if (abvMatch) {
         product.abv = abvMatch[1].replace(',', '.').replace(/\s+/g, '');
     }
+
+    const ldJson = await page.$$eval(
+        'script[type="application/ld+json"]',
+        nodes => nodes.map(n => n.innerText)
+    );
+
+    for (const json of ldJson) {
+        try {
+            const data = JSON.parse(json);
+
+            const productData =
+                data['@type'] === 'Product'
+                    ? data
+                    : Array.isArray(data)
+                    ? data.find(x => x['@type'] === 'Product')
+                    : null;
+
+            if (productData?.brand) {
+                product.producer =
+                    typeof productData.brand === 'string'
+                        ? productData.brand
+                        : productData.brand.name;
+                break;
+            }
+        } catch {}
+    }
     return product;
 }
