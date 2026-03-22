abbb6aa0bf6777571c9f8d526b785234d6868bf4
shokry1337 <emmess1337@gmail.com>
2026-03-22 03:37:03 +0200

    Update beerritz.co.uk

--- services/refiners/sites/beerritz.co.uk.js
+++ services/refiners/sites/beerritz.co.uk.js
@@ -1,13 +1,29 @@
 // services/refiners/sites/beerritz.co.uk.js
 export default async function refine(rootUrl, product, page) {
-    product.currency = 'GBP';
     product.country = 'UK';
-
+    product.currency = 'GBP';    
+    
     product.producer = await page.evaluate(() => {
         const li = Array.from(
             document.querySelectorAll('.product-summary-list li')
         ).find(el => el.textContent.startsWith('Brewery:'));
         return li ? li.textContent.replace('Brewery:', '').trim() : null;
+    });
+
+    product.abv = await page.evaluate(() => {
+        const li = Array.from(
+            document.querySelectorAll('.product-summary-list li')
+        ).find(el => el.textContent.startsWith('ABV:'));
+        return li ? li.textContent.replace('ABV:', '').trim() : null;
+    });
+
+    product.extras = product.extras || {};
+
+    product.extras.size = await page.evaluate(() => {
+        const li = Array.from(
+            document.querySelectorAll('.product-summary-list li')
+        ).find(el => el.textContent.startsWith('Unit Size:'));
+        return li ? li.textContent.replace('Unit Size:', '').trim() : null;
     });
 
     product.description = product.description
