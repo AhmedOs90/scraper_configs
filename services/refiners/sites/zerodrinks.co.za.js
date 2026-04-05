229d6f5b38416683da3df52ac2e941f4d9cc3c6a
shokry1337 <emmess1337@gmail.com>
2026-04-05 19:18:30 +0200

    Update zerodrinks.co.za

--- services/refiners/sites/zerodrinks.co.za.js
+++ services/refiners/sites/zerodrinks.co.za.js
@@ -2,5 +2,13 @@
 export default async function refine(rootUrl, product, page) {
     product.name = product.name.replace(' | Zero Drinks', '').trim();
     product.country = 'South Africa';
+
+    const ingredients = await page.evaluate(() => {
+        const el = document.querySelector('.brand-writeup .metafield-multi_line_text_field');
+        return el ? el.textContent.trim() : null;
+    });
+
+    product.extras = product.extras || {};
+    product.extras.ingredients = ingredients;
     return product;
 }
